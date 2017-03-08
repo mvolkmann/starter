78f7147348d7ae340fdcdc5400a78e9ac5890208
Tyler Haas <haasdevelopment@gmail.com>
2017-03-08 13:43:13 -0600

    revert to project formatting

--- src/error.js
+++ src/error.js
@@ -3,12 +3,12 @@
 type ResponseType = {
   message?: string,
   status: number
-}
+};
 
 export function handleError(url: string, res: ResponseType) {
   window.setState(
     res.status === 440 ?
       {error: 'Session Timeout', route: 'login'} :
       {error: res.message},
-  )
+  );
 }
