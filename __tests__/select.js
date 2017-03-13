b684f21a55293e289cd52ddc1f58b85156d2d9b9
Chris Grimes <cjg1981@live.com>
2017-03-13 09:54:32 -0500

    All of these are verified by the snapshot test

--- __tests__/select.js
+++ __tests__/select.js
@@ -13,43 +13,15 @@ describe('Select', () => {
   ];
   const size = 5;
   const value = 'hello';
-  const getDefaultProps = () => ({onChange, options, size, value});
+  const props = {onChange, options, size, value};
 
   it('should render', () => {
-    snapshot(<Select {...getDefaultProps()} />);
-  });
-
-  it('should have a className of .select on the select element', () => {
-    const wrapper = mount(<Select {...getDefaultProps()} />);
-    expect(wrapper.find('select.select').length).toEqual(1);
-    expect(wrapper.find('.select').length).toEqual(1);
-  });
-
-  it('should set the select list value via props.value', () => {
-    const wrapper = mount(<Select {...getDefaultProps()} />);
-    expect(wrapper.find('select').props().value).toEqual(value);
-  });
-
-  it('should set the select list size via props.size', () => {
-    const wrapper = mount(<Select {...getDefaultProps()} />);
-    expect(wrapper.find('select').props().size).toEqual(size);
+    snapshot(<Select {...props} />);
   });
 
   it('should call onChange when the select changes', () => {
-    const wrapper = mount(<Select {...getDefaultProps()} />);
+    const wrapper = mount(<Select {...props} />);
     wrapper.find('select').simulate('change', {target: {value: 'world'}});
     expect(onChange).toHaveBeenCalled();
-  });
-
-  it('should render the supplied options', () => {
-    const wrapper = mount(<Select {...getDefaultProps()} />);
-    const selectOptions = wrapper.find('option');
-
-    expect(selectOptions.length).toEqual(options.length);
-    for (let i = 0; i < options.length; i++) {
-      const selectOption = selectOptions.at(i);
-      expect(selectOption.text()).toEqual(options[i].text);
-      expect(selectOption.props().value).toEqual(options[i].value);
-    }
   });
 });
