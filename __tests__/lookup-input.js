32819e26ea43ac99e3280e02416cfbbc1d0a3bf4
Tyler Haas <haasdevelopment@gmail.com>
2017-03-13 15:59:01 -0500

    add file for testing

---
+++ __tests__/lookup-input.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import LookupInput from '../src/lookup-input'
+import {mount} from 'enzyme'
+import snapshot from './snapshot'
+
+describe('LookupInput', () => {
+})
+
---
+++ src/lookup-input.jsx
@@ -0,0 +1,48 @@
+import React, {PropTypes as t} from 'react'
+
+const styles = {
+  container: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  icon: {
+    position: 'relative',
+    flex: '1 1 0px',
+    left: '-20px'
+  },
+  input: {
+    width: '90%',
+    flex: '9 9 0px',
+    paddingRight: '32px'
+  }
+}
+
+const LookupInput = ({img, onChange, onClick, onKeyPress}) =>
+  (
+    <div className="lookup-input-container" style={styles.container}>
+      <input
+        className="form-control lookup-input-input"
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+        type="text"
+        style={styles.input}
+      />
+      <i
+        className={`lookup-input-icon fa fa-${img}`}
+        onClick={onClick}
+        style={styles.icon}
+      />
+    </div>
+  )
+
+
+LookupInput.displayName = 'LookupInput'
+
+LookupInput.propTypes = t.shape({
+  img: t.string.isRequired,
+  onChange: t.func.isRequired,
+  onClick: t.func,
+  onKeyPress: t.func
+}).isRequired
+
+export default LookupInput
