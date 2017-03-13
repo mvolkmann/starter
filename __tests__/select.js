76313945557370126b6094b3a4093aa501a8cd69
Chris Grimes <cjg1981@live.com>
2017-03-13 09:49:44 -0500

    Use actual component name

--- __tests__/select.js
+++ __tests__/select.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {mount} from 'enzyme';
 
-import Component from '../src/select';
+import Select from '../src/select';
 import snapshot from './snapshot';
 
-describe(Component.name, () => {
+describe('Select', () => {
   const onChange = jest.fn();
   const options = [
     {text: 'A', value: 'a'},
@@ -16,33 +16,33 @@ describe(Component.name, () => {
   const getDefaultProps = () => ({onChange, options, size, value});
 
   it('should render', () => {
-    snapshot(<Component {...getDefaultProps()} />);
+    snapshot(<Select {...getDefaultProps()} />);
   });
 
   it('should have a className of .select on the select element', () => {
-    const wrapper = mount(<Component {...getDefaultProps()} />);
+    const wrapper = mount(<Select {...getDefaultProps()} />);
     expect(wrapper.find('select.select').length).toEqual(1);
     expect(wrapper.find('.select').length).toEqual(1);
   });
 
   it('should set the select list value via props.value', () => {
-    const wrapper = mount(<Component {...getDefaultProps()} />);
+    const wrapper = mount(<Select {...getDefaultProps()} />);
     expect(wrapper.find('select').props().value).toEqual(value);
   });
 
   it('should set the select list size via props.size', () => {
-    const wrapper = mount(<Component {...getDefaultProps()} />);
+    const wrapper = mount(<Select {...getDefaultProps()} />);
     expect(wrapper.find('select').props().size).toEqual(size);
   });
 
   it('should call onChange when the select changes', () => {
-    const wrapper = mount(<Component {...getDefaultProps()} />);
+    const wrapper = mount(<Select {...getDefaultProps()} />);
     wrapper.find('select').simulate('change', {target: {value: 'world'}});
     expect(onChange).toHaveBeenCalled();
   });
 
   it('should render the supplied options', () => {
-    const wrapper = mount(<Component {...getDefaultProps()} />);
+    const wrapper = mount(<Select {...getDefaultProps()} />);
     const selectOptions = wrapper.find('option');
 
     expect(selectOptions.length).toEqual(options.length);
