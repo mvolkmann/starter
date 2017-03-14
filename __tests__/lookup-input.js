7236a0fec51af29a1afbe2ad92575119eaca3265
Tyler Haas <haasdevelopment@gmail.com>
2017-03-14 13:17:53 -0500

    (LookupInput) fix tests to match changes to component

--- __tests__/lookup-input.js
+++ __tests__/lookup-input.js
@@ -21,7 +21,7 @@ describe('LookupInput', () => {
 
   it('should call onClick fn when the icon is clicked', () => {
     const onClick = jest.fn();
-    const wrapper = mount(<LookupInput img="search" onClick={onClick} />);
+    const wrapper = mount(<LookupInput img="search" onSubmit={onClick} />);
     const icon = wrapper.find('.lookup-input-icon');
     icon.simulate('click');
     expect(onClick).toHaveBeenCalled();
@@ -29,7 +29,7 @@ describe('LookupInput', () => {
 
   it('should call onClick fn when enter key is pressed', () => {
     const onEnterPress = jest.fn();
-    const wrapper = mount(<LookupInput img="search" onClick={onEnterPress} />);
+    const wrapper = mount(<LookupInput img="search" onSubmit={onEnterPress} />);
     const input = wrapper.find('.lookup-input-input');
     input.simulate('keyPress', {which: 13});
     expect(onEnterPress).toHaveBeenCalled();
