7ff61762e76a423aed1e037c65b9495613d99af1
Tyler Haas <haasdevelopment@gmail.com>
2017-03-14 16:38:45 -0500

    (WizardSteps) create initial version of component
    create component to allow a visual queue to user of where they are in
    the wizards workflow

---
+++ src/share/wizard-steps.js
@@ -0,0 +1,24 @@
+// @flow
+import React, {PropTypes as t} from 'react'
+
+type PropsType = {steps: Array<string>}
+
+const WizardSteps = ({steps}: PropsType) =>
+  (
+    <div className="wizard-steps-container">
+      {
+        steps.map((s, i) =>
+          (<div className="step-container" key={i}>
+            <span className="badge">{i}</span> {s}
+          </div>))
+      }
+    </div>
+  )
+
+WizardSteps.displayName = 'WizardSteps'
+
+WizardSteps.propTypes = {
+  steps: t.arrayOf(t.string)
+}
+
+export default WizardSteps
