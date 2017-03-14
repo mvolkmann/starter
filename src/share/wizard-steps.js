4e4e2f4bbd9931c4fb8cb9079781b341ed328406
Tyler Haas <haasdevelopment@gmail.com>
2017-03-14 16:41:56 -0500

    format with prettier

--- src/share/wizard-steps.js
+++ src/share/wizard-steps.js
@@ -3,22 +3,20 @@ import React, {PropTypes as t} from 'react'
 
 type PropsType = {steps: Array<string>}
 
-const WizardSteps = ({steps}: PropsType) =>
-  (
-    <div className="wizard-steps-container">
-      {
-        steps.map((s, i) =>
-          (<div className="step-container" key={i}>
-            <span className="badge">{i}</span> {s}
-          </div>))
-      }
-    </div>
-  )
+const WizardSteps = ({steps}: PropsType) => (
+  <div className="wizard-steps-container">
+    {steps.map((s, i) => (
+      <div className="step-container" key={i}>
+        <span className="badge">{i}</span> {s}
+      </div>
+    ))}
+  </div>
+)
 
 WizardSteps.displayName = 'WizardSteps'
 
 WizardSteps.propTypes = {
-  steps: t.arrayOf(t.string)
+  steps: t.arrayOf(t.string),
 }
 
 export default WizardSteps
